8ff1bcbd7704310882108bae6de782d223333498
Naofumi MURATA <10278628+nao23@users.noreply.github.com>
2023-01-08 22:48:56 +0900

    puts builtin function

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -1,6 +1,9 @@
 package evaluator
 
-import "github.com/nao23/monkey/object"
+import (
+	"fmt"
+	"github.com/nao23/monkey/object"
+)
 
 var builtins = map[string]*object.Builtin{
 	"len": &object.Builtin{
@@ -88,6 +91,15 @@ var builtins = map[string]*object.Builtin{
 			newElements[length] = args[1]
 
 			return &object.Array{Elements: newElements}
+		},
+	},
+	"puts": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			for _, arg := range args {
+				fmt.Println(arg.Inspect())
+			}
+
+			return NULL
 		},
 	},
 }
